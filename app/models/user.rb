6e91f0deaf00be683c8d29339f56d61345f34835
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-05-30 17:37:26 +0200

    refactor validation  of user name and email fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates_presence_of :name
-    validates_presence_of :email
+    validates :name, presence: true
+    validates :email, presence: true
 end
